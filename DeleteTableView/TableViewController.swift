41f236ebc6c56a6b8d7fea8617e22dac1d61aef9
Hongchinh97 <chinhuv@gmail.com>
2019-06-22 09:28:54 +0700

    add

--- DeleteTableView/TableViewController.swift
+++ DeleteTableView/TableViewController.swift
@@ -12,8 +12,9 @@ class TableViewController: UITableViewController {
 
     @IBOutlet var noDataView: UIView!
     @IBOutlet weak var footerView: UIView!
+    var isDone: Bool = false
     
-    var number: [Int] = []
+    var number: [Int] = Array(1...5)
     var hasNoData: Bool = false {
         didSet {
                hasNoData ? (tableView.tableFooterView = noDataView) : (tableView.tableFooterView = footerView)
@@ -28,38 +29,68 @@ class TableViewController: UITableViewController {
         
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        hasNoData = (number.count == 0)
+//        hasNoData = (number.count == 0)
+        tableView.reloadData()
     }
     
-   
-    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: true)
+        tableView.isEditing = editing
+        
+        
+    }
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if isEditing {
+            return false
+        } else {
+            return true
+        }
+    }
     
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return number.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        // Configure the cell...
         cell.textLabel?.text = String(number[indexPath.row])
         return cell
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let index  = tableView.indexPathForSelectedRow {
-            if let viewTableController = segue.destination as? ViewController {
-                viewTableController.name = String(number[index.row])
-            }
+            if let index  = tableView.indexPathForSelectedRow {
+                if let viewTableController = segue.destination as? ViewController {
+                    viewTableController.name = String(number[index.row])
+                }
         }
+        
     }
+
+    @IBAction func actionDelete(_ sender: UIButton) {
+        var items = [Int]()
+        if let selectedRow = tableView.indexPathsForSelectedRows {
+            for item in items{
+                if let index = number.index(of: item) {
+                    number.remove(at: index)
+                    
+                }
+            }
+            print(selectedRow)
+//            tableView.endUpdates()
+            tableView.deleteRows(at: selectedRow, with: .fade)
+            tableView.reloadData()
+        }
+        
+        
+    }
+    
+    
+    
     
     @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
         if let viewController = unwindSegue.source as? ViewController {
@@ -73,20 +104,6 @@ class TableViewController: UITableViewController {
         }
         tableView.reloadData()
         
-        //        if let index1 = tableView.indexPathForSelectedRow {
-        //            if let viewTable = unwindSegue.source as? ViewController {
-        //                if viewTable.name != nil && Int(viewTable.name!) != nil {
-        //                    number[index1.row] = Int(viewTable.name!)!
-        //                }
-        //            }
-        //
-        //        } else {
-        //            if let viewTable2 = unwindSegue.source as? ViewController {
-        //                if let name = viewTable2.name {
-        //                    number.append(Int(name)!)
-        //                }
-        //            }
-        //        }
     }
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -98,13 +115,8 @@ class TableViewController: UITableViewController {
             number.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
-       // cach 1
-//                if number.count == 0 {
-//                    hasNoData = true
-//                } else {
-//                    hasNoData = false
-//                }
-        hasNoData = (number.count == 0)
        
+        hasNoData = (number.count == 0)
+       tableView.reloadData()
     }
 }
